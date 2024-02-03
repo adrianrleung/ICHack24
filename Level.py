f94f6aed22ce904b7a8cd1dc613203928a963ea9
AlexTClarke <119004428+AlexTClarke@users.noreply.github.com>
2024-02-03 21:36:00 +0000

    Update Level.py

--- Level.py
+++ Level.py
@@ -3,26 +3,23 @@ import json
 
 class Level:
     def __init__(self, filename):
-        data = self.importJSON(filename)
-        self.data = data
-        self.choices = data["choices"]
-        self.correct = data["correct"]
-        self.description = data["description"]
+        self.data = self.importJSON(filename)
+        self.choices = self.data["choices"]
+        self.correct = self.data["correct"]
+        self.description = self.data["description"]
 
     
     def launchLevel(self):
         for line in self.description:
             print("System: " + line)
         print("Your choices: ")
-        for choice in self.choices:
-            print(choice)
-        userChoice = input("enter your choice")
-        while userChoice > len(self.choices) or userChoice < 0:
-            userChoice = input("invalid. Try again")
-        if userChoice == self.correct:
-            return True
-        else:
-            return False
+        for index,choice in enumerate(self.choices):
+            print(str(index+1)+".",choice)
+        userChoice = input("enter your choice: ")
+        while userChoice > len(self.choices) or userChoice > 1 or not userChoice.isdigit():
+            userChoice = print("Invalid. Try again")
+        userChoice = int(userChoice)-1 # Changes from 1 indexing to 0 indexing
+        return userChoice == self.correct
         
     def importJSON(self, filename):
         with open(filename) as f:
