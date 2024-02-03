81270d433303fe59818b7e84100dea70b05d2090
Adrian <al4523@ic.ac.uk>
2024-02-03 21:04:33 +0000

    started levels

--- Level.py
+++ Level.py
@@ -1,3 +1,32 @@
-class interaction:
-    def __init__(self, text):
-        self.text = text
+import Tree
+
+# class textBlock:
+#     def __init__(self, text):
+#         self.text = text
+
+# class Decision:
+#     def __init__(self, text)
+
+class Level:
+    def __init__(self, data):
+        self.data = data
+
+    
+    def launchLevel(self):
+        for line in data[description]:
+            print("System " + text)
+        for choice in data[choices]:
+            userChoice = input(choice)
+            while userChoice.upper() not in ["Y","N"]:
+                userChoice = input("invalid input, try again")
+            if userChoice != data[correct]:
+                self.fail()
+            else:
+                self.corrects.pop(0)
+        self.right()
+    
+    def fail(self):
+        pass
+
+    def right(self):
+        pass
